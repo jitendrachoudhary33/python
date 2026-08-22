c43dbb2ce42d19a6f13d240d7dd263d948f3e95a
jitendrachoudhary33 <jitendra.choudhary.cg@gmail.com>
2026-08-22 16:58:05 +0530

    python variable assignment added

---
+++ 0.1 python variable/assignment.py
@@ -0,0 +1,18 @@
+
+name="jitesh"
+age=19
+city="alwar"
+print(name,age,city)
+# question 17
+student_name="jitesh"
+student_age=19
+student_city="alwar"
+total_marks =550
+collage_name="SNU"
+print(student_name,student_age,student_city,total_marks,collage_name)
+# question=30
+student_name="daksh"
+student_age=18
+student_age=19
+course="B.tech"
+print(student_name,student_age,course)
