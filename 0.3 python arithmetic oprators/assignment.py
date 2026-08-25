42598f79e3aae2ff5555a7e5c91a9d86ca6fbf27
jitendrachoudhary33 <jitendra.choudhary.cg@gmail.com>
2026-08-25 23:43:16 +0530

    python arithmetic file added

---
+++ 0.3 python arithmetic oprators/assignment.py
@@ -0,0 +1,170 @@
+# Task 1
+a=9
+b=8
+#print(a+b)
+#print(a*b)
+#print(a/b)
+#print(a//b)
+#print(a%b)
+#print(a**b)
+# Task 2
+a=6
+b=4.5
+#print(a+b)
+#print(a*b)
+#print(a/b)
+#print(a//b)
+#print(a%b)
+#print(a**b)
+# Task 3
+html_marks=200
+python_marks=250
+css_marks=300
+#print(html_marks+python_marks+css_marks)
+#print((html_marks+python_marks+css_marks)/2)
+# Task 4
+price=2500
+quantity=3
+#print(price*quantity)
+# Task 5
+A=4
+B=7
+#print(A/2) #divisible by 2 hence, even
+#print(B/2)# not divisible by 2 hence, odd
+# Task 6
+x=9
+y=-2
+#print(x/y)
+#print(x//y)
+# Task 7
+x=-3
+y=-8
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x/y)
+#print(x//y)
+#print(x%y)
+# Task 8 
+#print(9-7)
+#print(9-(-7))
+#print(-7-9)
+#print(-9-(-7))
+#  Task 9
+#print(9//7)
+#print(-9//7)
+#print(9//-7)
+#print(-9//-7)
+#the negative results are different from simply removing the decimal part because floor division round off the value from positive infinity to negative infinity
+# Task 10
+#print(9%7)
+#print(-9%7)
+#print(9%-7) 
+#print(-9%-7)
+# Task 11
+#(1) 10+5*2 (here, first priority is to solve multiply then addition so, ans. is 20)
+#print(10+5*2)
+#(2) 20-4/2 (here, first priority is to solve division then substraction so, ans. is 18.0)
+#print(20-4/2)
+#(3) 10+20/5*2 (here, first priority is to solve multiplication then divison after that addition so, ans. is 18)
+#print(10+20/5*2)
+#(4) 2+3*4**2 (here, first priority is to solve exponential then multiply after that addition so, ans. is 50)
+#print(2+3*4**2)
+#(5) 100-20//5 (here, first priority is to solve floor division then substraction so, ans. is 96)
+#print(100-20//5)
+# Task 12 
+#print(10+5*2) # 1st
+#print((10+5)*2) # 2nd
+# in first priority of multipication then addition while in 2nd priority goes to parenthesis then multiplication.
+#print(20-10/2)
+#print((20-10)/2)
+# in 1st priority of division then substraction while in 2nd priority goes to parenthesis then division 
+#print(2+3*4)
+#print((2+3)*4)
+# Task 13 
+#print(True+True)
+#print(True+False)
+#print(False+True)
+#print(False+False)
+#print(True-True)
+#print(True*False)
+#print(True/True)
+#print(False//True)
+#print(True%True)
+# Task 14
+#print(True+5)
+#print(False+5)
+#print(True*10)
+#print(False*10)
+#print(True-5)
+#print(False-5)
+# here, in python True means "1" and False means "0"
+a="jitesh"
+b="choudhary"
+#print(a+" "+b)
+p="python"
+#print(4*p)
+#print(2.5*p)
+# non-init type cannot run this oprator
+# Task 17
+#print("aman"+"singh")
+#print("daksh"-"khandelwal")
+#print("naman"*3)
+#print("jainam"/"jain")
+# Task 18
+value=None
+#string(value+2)
+#string(value-3)
+#string(value*3)
+#string(value/3)
+#string(value//3)
+#string(value%3)
+#string(value**3)
+a=4
+b=5
+#print(a+b)
+#print(a-b)
+#print(a*b)
+#print(a/b)
+#print(a//b)
+#print(a%b)
+#print(a**b)
+# 1. Addition
+# Predict: 12.5 
+print(10+2.5)
+
+# 2. Subtraction (with negative number)
+# Predict: 13 
+print(10-(-3))
+
+# 3. Multiplication
+# Predict: -7.5 
+print((-3)*2.5)
+
+# 4. Division 
+# Predict: -3.3333333333333335 
+print(10/(-3))
+
+# 5. Floor Division 
+# Predict: -4 
+print(10//(-3))
+
+# 6. Modulus 
+# Predict: -2 
+print(10%(-3))
+
+# 7. Exponentiation
+# Predict: 0.001
+print(10**(-3))
+
+# 8. Multiple operators
+# Predict: 10 + (-7.5) = 2.5 
+print(10+(-3)*2.5)
+
+# 9. Multiple operators
+# Predict: 7 / 2.5 = 2.8 
+print((10+(-3))/2.5)
+
+# 10. Multiple operators
+# Predict: 10 // 9 = 1 
+print(10//((-3)**2))
