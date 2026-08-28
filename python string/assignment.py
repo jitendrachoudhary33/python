57e36e031d651b8b537bfcf34747a347b1ce5c93
jitendrachoudhary33 <jitendra.choudhary.cg@gmail.com>
2026-08-28 23:17:41 +0530

    python string file added

---
+++ python string/assignment.py
@@ -0,0 +1,102 @@
+# Task=1
+my_name="jitesh"
+city="alwar"
+favorite_promming_language="python"
+print(my_name,city,favorite_promming_language)
+# Task=2
+a="   "
+print(a)
+print(len(a))
+print(type(a))
+# Task=3
+a="Python Programming"
+print(a)
+print(len(a))
+print(a[0])
+print(a[-1])
+print(a[2])
+print(a[-2])
+# Task=4
+a="programming"
+print(a[0])
+print(a[1])
+print(a[6])
+print(a[-1])
+# Task=5
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-11])
+# Task=6
+a="jitesh choudhary"
+print(a[0])
+print(a[-1])
+print(a[7])
+# Task=7
+a="Python Programming"
+print(a[:6])
+print(a[7:])
+print(a)
+print(a[:5])
+print(a[-5:])
+# Task=8
+a="ABCDEFGHIJKL"
+print(a[::2])
+print(a[::3])
+print(a[1:8:2])
+print(a[::-1])
+# Task=9
+a="Python Programming"
+print(a[-5:])
+print(a[-10:])
+print(a[::-1])
+# Task=10
+a="programming"
+print(a[:3])
+print(a[-3:])
+print(a[::2])
+print(a[-1])
+print(a[1:10])
+# Task=11
+a="python"
+b="pyhtonprogramming"
+c="my fevorite language is python"
+print(len(a))
+print(len(b))
+print(len(c))
+# Task=12
+text="Python Programming"
+print(len(text))
+print(text[17])
+# Task=13
+first_name="jitesh"
+last_name="choudhary"
+print(first_name + " " + last_name)
+# Task=14
+Name="jitesh"
+Age=19
+City="alwar"
+Programming_language="python"
+print(Name +str(Age)+City+Programming_language)
+# Task=15
+#print(5+"guild")
+# type error occurs
+print(str(5)+"guild")
+# Task=16
+a="Hlo "
+print(a*3)
+print(a*5)
+print(a*10)
+# Task=17
+a="*"
+print(a*10)
+# Task=19
+a="python programming language"
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.title())
+print(a.swapcase())
+# Task=20
+a="Python is a programming language"
+print(a in "Python")
