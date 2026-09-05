164a55222907bfc3a1ba5c1f74c47446c19fb761
jitendrachoudhary33 <jitendra.choudhary.cg@gmail.com>
2026-09-05 23:53:19 +0530

    file added

--- 0.6 python input-output/assignment.py
+++ 0.6 python input-output/assignment.py
@@ -1,21 +1,106 @@
-# Task = 1
-name = input("Enter your name: ")
-print(name)
+# # Task = 1
+# name = input("Enter your name: ")
+# print(name)
 
-# Task = 2
-city=input("Enter your city :")
-print(f"your city is {city}")
+# # Task = 2
+# city=input("Enter your city :")
+# print(f"your city is {city}")
 
-# Task = 3
-user_name = input("Enter your name :" )
-age = input("Enter your age :")
-print (user_name,age)
+# # Task = 3
+# user_name = input("Enter your name :" )
+# age = input("Enter your age :")
+# print (user_name,age)
 
-# Task = 4
-text = input("Enter today's date :")
-print(type(text))
+# # Task = 4
+# text = input("Enter today's date :")
+# print(type(text))
 
-# Task = 5
-value = input("Enter any value :")
-print(type(value))
+# # Task = 5
+# value = input("Enter any value :")
+# print(type(value))
 
+# # Task = 6
+# first_name = input("Enter your first name :")
+# last_name = input("Enter your last name :")
+# print(first_name,last_name,sep="-")
+
+# # Task = 7
+# name = input("Enter your  name :")
+# collage = input("Enter your collage :")
+# city = input("Enter your city :")
+# print(name,"\n",collage,"\n",city)
+
+# Task = 8
+# first_name,last_name = input("Enter your first and last name:").split()
+# print(first_name)
+# print(last_name)
+
+# # Task = 9
+# a , b = input("Enter some text:").split() # here, we assign two variable a value seprated by commas(,).
+# print(a)
+# print(b)
+
+# # Task = 10
+# a,b,c = input("Enter three words:").split()
+# print(a)
+# print(b)
+# print(c)
+
+# # Task = 11
+# a = int("25")
+# a = int(a)
+# print(type(a))
+
+# # Task = 12
+# a = float("25.5")
+# print(type(a))
+
+# # Task = 13
+# a = str(100)
+# print(type(a))
+
+# # Task = 14
+# value = int(input("Enter some value :"))
+# print(type(value))
+
+# # Task = 15
+# float = float(input("Enter some number:"))
+
+# print(type(float))
+
+# # Task = 16
+# a = input("Enter first name:")
+# b = input("Enter last name:")
+
+# print(a+b)
+#              # when we add instead of numeric addition concetination occurs bcz input by default give the datatype string   
+
+# # Task = 17
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print(a + b)
+
+# # Task = 18
+# name = "Rahul"
+# age = 20
+
+# print(f"My name is {name} and I am {age} years old.")
+
+# # Task = 19
+# a = 10
+# b = 20
+
+# print(f"sum of a and b is {a+b}")
+
+# # Task = 20
+# name = input("Enter your name:")
+# age = input("Enter your age :")
+
+# print(f"Your name is {name},You are {age} years old.")
+
+# Task = 21
+price = float(95.23343)
+print(price(f"Price is :.2f"))
+
+# Task = 22
