17fe0dc9dad944815cb384cd846305b38d1cb741
jitendrachoudhary33 <jitendra.choudhary.cg@gmail.com>
2026-09-09 23:32:41 +0530

    for loop till 15

---
+++ 0.8 for-loop/assignment.py
@@ -0,0 +1,101 @@
+# # Question = 1
+# for i in range (1,6) :
+#     print("Hello")
+
+
+# # Question = 2
+# for i in range (0,10) :
+#     print(i)
+
+
+# # Question = 3
+# for i in range (1,11) :
+#     print(i)
+
+
+# # Question = 4
+# for i in range (10,0,-1) :
+#     print(i)
+
+
+# # Question = 5
+# for i in range (5,51,5) :
+#     print(i)
+
+
+# # Question = 6
+# for i in range (2,21) :
+#     if i % 2 == 0 :
+#         print(f"Even is : {i} ")
+
+
+# # Question = 7
+# for num in range (1,20) :
+#     if num % 2 == 1 :
+#         print(f"Odd is : {num} ")
+
+
+# # Question = 8
+# for num in range (1,7) :
+#     print(num*3)
+
+
+# # Question = 9
+# for num in range (20,1,-2) :
+#     print(num)
+
+
+# # Question = 10
+# num = int(input("Enter a positive integer :"))
+# for i in range (1,num+1) :
+#     print(i)
+
+
+# # Question = 11
+# num = int(input("Enter a positive integer :"))
+# for i in range (1,num+1) :
+#     if i % 2 == 0 :
+#         print(f"Even is : {i}")
+
+
+# # Question = 12
+# num = int(input("Enter a positive integer :"))
+# for i in range (1,num+1) :
+#     if i % 2 == 1 :
+#         print(f"Odd is : {i}")
+
+
+# # Question = 13
+# num = int(input("Enter a limit :"))
+# for i in range (1,num+1) :
+#     if i % 3 == 0 :
+#         print(f" {i} : Divisible by 3")
+
+
+# # Question = 14 
+# n = int(input("Enter a limit :"))
+# for i in range (1,n+1) :
+#     if i % 2 == 0 and i % 3 == 0 :
+#         print(f" {i} : Either divisible by 2 or 3")
+
+
+# # Question = 15 
+# n = int(input("Enter a limit :"))
+# count = 0 
+# for i in range (1,n+1) :
+#     if i % 2 == 0 :
+#         count = count + 1
+# print(count)
+
+
+# # Question = 16
+# n = int(input("Enter a number :"))
+# total = 0
+# for i in range (1,n+1) : 
+#     print(total + i)
+
+
+
+        
+
+    
